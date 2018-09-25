d16d0f8055ba7e16c46b06e9d7e8b3a973cf93b9
Athila Naniny Ninow <athila@catamoeda.com.br>
2018-09-25 09:27:48 -0300

    express fix get

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('port', process.env.PORT || 8080);
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
 
 const server = app.listen(app.get('port'), function () {
     console.log('listening on port ', server.address().port);
